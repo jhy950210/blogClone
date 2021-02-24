42c994dba1d17039eb6112a3a15e7b723d665539
jhy950210 <jhyy1995210@gmail.com>
2021-02-24 22:49:33 +0900

    회원가입/수정/세션 - 스프링 작동원리 학습.

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -4,6 +4,13 @@ let index = {
 			this.save();
 		});
 		
+		$("#btn-delete").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!
+			this.deleteById();
+		});
+		
+		$("#btn-update").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!
+			this.update();
+		});
 	},
 	
 	save: function(){
@@ -25,8 +32,48 @@ let index = {
 		alert(JSON.stringify(error));
 	});
 	
-	}
+	},
 	
+	deleteById: function(){
+		let id = $("#id").text();
+		
+		$.ajax({
+			type:"DELETE",
+			url:"/api/board/" + id,
+			contentType:"application/json; charset=utf-8", // body데이터가 어떤 타입인지
+			dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript오브젝트로 변경
+		}).done(function(response){
+			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	
+	},
+	
+	update: function(){
+		let id = $("#id").val();
+		
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+		console.log(id);
+		console.log(data);
+		$.ajax({
+			type:"PUT",
+			url:"/api/board/" + id,
+			data: JSON.stringify(data), // http body데이터
+			contentType:"application/json; charset=utf-8", // body데이터가 어떤 타입인지
+			dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript오브젝트로 변경
+		}).done(function(response){
+			alert("글수정이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+		
+	}
 }
 
 index.init();
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,6 +4,10 @@ let index = {
 			this.save();
 		});
 		
+		$("#btn-update").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!
+			this.update();
+		});
+		
 	},
 	
 	save: function(){
@@ -25,6 +29,29 @@ let index = {
 		dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript오브젝트로 변경
 	}).done(function(response){
 		alert("회원가입이 완료되었습니다.");
+		location.href = "/";
+	}).fail(function(error){
+		alert(JSON.stringify(error));
+	});// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+	
+	},
+	
+	update: function(){
+		let data = {
+			id: $("#id").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+	};
+	
+	// ajax호출시 default가 비동기 호출
+	$.ajax({
+		type:"PUT",
+		url:"/user",
+		data: JSON.stringify(data), // http body데이터
+		contentType:"application/json; charset=utf-8", // body데이터가 어떤 타입인지
+		dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript오브젝트로 변경
+	}).done(function(response){
+		alert("회원수정이 완료되었습니다.");
 		location.href = "/";
 	}).fail(function(error){
 		alert(JSON.stringify(error));
